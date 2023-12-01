7b0359f2b524abbb53da98cb84c1c848ac6ccc12
Lane Schmelzenbach <lschmelzenbach@relias.com>
2023-12-01 13:25:53 -0700

    working

--- app.py
+++ app.py
@@ -3,31 +3,38 @@ import openai
 import os 
 from dotenv import load_dotenv,find_dotenv
 
-__ = load_dotenv(find_dotenv()) #read local .env file
-openai.api_key=os.environ["OPENAI_API_KEY"]
+load_dotenv(find_dotenv())
+openai.api_type = "azure"
+openai.api_base = "https://reliasopenaitesting.openai.azure.com/"
+openai.api_version = "2023-07-01-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 app.static_folder = 'static'
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/get")
 def get_bot_response():
-    
     userText = request.args.get('msg')
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": userText}
+    message_text = [
+        {"role":"system","content":"You are an AI assistant that helps people find information."},
+        {"role":"user","content": userText}
     ]
+    completion = openai.ChatCompletion.create(
+        engine="ChatGPT4LargeRelias",
+        messages=message_text,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
     )
-    result=completion.choices[0].message['content']
- 
+    result = completion.choices[0].message['content']
     return result
-   
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8002,debug=True)
